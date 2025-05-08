ab37e9e5b53893bfbe238a76007b71fb29225edb
mateo729 <mateovalencia211@gmail.com>
2025-05-08 16:56:25 -0500

    primer
    comienzo de la base de datos

---
+++ barberriasema.dml.sql
@@ -0,0 +1,15 @@
+insert into categoria values(1,"Corte y Estilo");
+
+insert into categoria (nombre) values("Afeitado y Barba"),(" Tratamientos Capilares"),("Masajes y Relajación"),("Cuidado Personal y Estético")
+
+/*insert into Clientes (cliente_id, nombre, apellido, telefono, correo, fecha_registro) value
+(1, "eduardo", "forero", 301254125, "lolalamento@gmail.com", '2025-05-02' ),
+(2, "carlos", "puerto", 3021545745, "diasfeli@gmail.com", '2025-05-06' ),
+(3, "omar", "lopez", 302151245, "soloyo@gmail.com", '2025-05-08'),
+(4, "lucho", "martinez", 3025897454, "koquito@gmail.com", '2025-05-09');
+
+insert into Empleados (empleado_id, nombre, apellido, telefono, especialidad, telefono, correo, fecha_contratacion) value 
+(1, "camilo", "herrera", 302154784, "nonomo@barber.com", '2024-01-05'),
+(2, "nico", "perez", 302445741, "nicocnio@barber.com", '2024-01-06'),
+(3, "roro", "paez", 325674845, "paezdos@barber.com", '2024-05-10'),
+(4, "luis", "mena", 302568794, "luismena@barber.com", '2024-02-13');*/
